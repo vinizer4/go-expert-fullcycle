d962795c82ba69c166e2ee403802c5c3a5fadb92
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2025-01-25 14:04:18 -0300

    private and public methods

--- fundation/packages/main.go
+++ fundation/packages/main.go
@@ -9,4 +9,7 @@ func main() {
 	sum := math.Sum(10, 20)
 
 	fmt.Println(sum)
+
+	car := math.Car{Brand: "BMW"}
+	fmt.Println(car.Run())
 }
--- fundation/packages/math/math.go
+++ fundation/packages/math/math.go
@@ -1,5 +1,33 @@
 package math
 
+// If you want to make a function public, the first letter of the function must be uppercase
+
+// Sum is a function that receives two parameters and returns the sum of them
 func Sum[T int | float64](a, b T) T {
 	return a + b
+}
+
+// if the first letter of the function tiny "minuscule" this function is private
+func sum[T int | float64](a, b T) T {
+	return a + b
+}
+
+// in a struct is the same case the first letter of the struct must be uppercase to make it public
+// example
+
+// This is a public struct, is possible to import this struct in another package
+
+// Car is a struct that represents a car
+type Car struct {
+	Brand string
+}
+
+// This is a private struct, is not possible to import this struct in another package
+
+type car struct {
+	brand string
+}
+
+func (c Car) Run() string {
+	return "The car is running"
 }
