8dc5277520788d5d9d6ee5cd582ea8fd562e121b
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2025-03-02 18:08:09 -0300

    working with server mux

---
+++ important-packages/server-mux/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HomeHandler)
+	mux.Handle("/blog", blog{title: "My Blog"})
+	http.ListenAndServe(":8080", mux)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Home"))
+}
+
+type blog struct {
+	title string
+}
+
+func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(b.title))
+}
