ef58f96eb5ee2bb5c10d05f83223ef6605553b3b
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2025-03-08 14:17:16 -0300

    insert data into database

--- database/main.go
+++ database/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
@@ -8,10 +12,36 @@ type Product struct {
 	Price float64
 }
 
-func NewProduct(id, name string, price float64) *Product {
+func NewProduct(name string, price float64) *Product {
 	return &Product{
 		ID:    uuid.New().String(),
 		Name:  name,
 		Price: price,
 	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	product := NewProduct("Laptop", 1000)
+	err = insertProduct(db, *product)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
+
+func insertProduct(db *sql.DB, product Product) error {
+	stmt, err := db.Prepare("INSERT INTO products(id, name, price) VALUES(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
 }
