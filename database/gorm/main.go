0d8a0eb6520e6a0df783f10be478f1fad8389ec6
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2025-03-08 14:38:54 -0300

    initial config GORM

---
+++ database/gorm/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID    int `gorm:"primary_key"`
+	Name  string
+	Price float64
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&Product{})
+}
