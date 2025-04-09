9789b216dab57e5e2fd01364c16c133d33c7987b
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2025-04-08 22:42:37 -0300

    APIs module: implemented find and update products

--- apis/internal/infra/webserver/handlers/product_handlers.go
+++ apis/internal/infra/webserver/handlers/product_handlers.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"github.com/go-chi/chi/v5"
 	"github.com/vinizer4/go-expert-fullcycle/apis/internal/dto"
 	"github.com/vinizer4/go-expert-fullcycle/apis/internal/entity"
 	"github.com/vinizer4/go-expert-fullcycle/apis/internal/infra/database"
+	entityPkg "github.com/vinizer4/go-expert-fullcycle/apis/pkg/entity"
 	"net/http"
 )
 
@@ -36,4 +38,49 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product, err := h.ProductDB.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
+}
+
+func (h *ProductHandler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var product entity.Product
+	err := json.NewDecoder((r.Body)).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product.ID, err = entityPkg.ParseID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	_, err = h.ProductDB.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	err = h.ProductDB.Update(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
