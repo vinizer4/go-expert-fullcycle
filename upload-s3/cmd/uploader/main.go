f069f7014801cb41a7fc84c073227de04b4ee1cc
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2025-07-20 11:21:13 -0300

    upload S3 module: finished upload s3 module

--- upload-s3/cmd/uploader/main.go
+++ upload-s3/cmd/uploader/main.go
@@ -8,11 +8,13 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"io"
 	"os"
+	"sync"
 )
 
 var (
 	s3Client *s3.S3
 	s3Bucket string
+	wg       sync.WaitGroup
 )
 
 func init() {
@@ -20,8 +22,8 @@ func init() {
 		&aws.Config{
 			Region: aws.String("us-east-1"),
 			Credentials: credentials.NewStaticCredentials(
-				"EXAMPLEACCESSKEY",
-				"EXAMPLESECRETKEY",
+				"EXAMPLEACCESSKEYID",
+				"EXAMPLESECRET",
 				"",
 			),
 		},
@@ -30,7 +32,7 @@ func init() {
 		panic(err)
 	}
 	s3Client = s3.New(sess)
-	s3Bucket = "goexpert-bucket-exemplo"
+	s3Bucket = "goexpert-bucket-exemplo-vini"
 }
 
 func main() {
@@ -39,6 +41,21 @@ func main() {
 		panic(err)
 	}
 	defer dir.Close()
+	uploadControl := make(chan struct{}, 5)
+	errorFileUpload := make(chan string, 5)
+
+	go func() {
+		for {
+			select {
+			case fileName := <-errorFileUpload:
+				fmt.Printf("Error uploading file: %s\n", fileName)
+				uploadControl <- struct{}{}
+				wg.Add(1)
+				go uploadFile(fileName, uploadControl, errorFileUpload)
+			}
+		}
+	}()
+
 	for {
 		files, err := dir.ReadDir(1)
 		if err != nil {
@@ -48,16 +65,22 @@ func main() {
 			fmt.Sprintf("Error reading directory: %v\n", err)
 			continue
 		}
-		uploadFile(files[0].Name())
+		wg.Add(1)
+		uploadControl <- struct{}{}
+		go uploadFile(files[0].Name(), uploadControl, errorFileUpload)
 	}
+	wg.Wait()
 }
 
-func uploadFile(fileName string) {
+func uploadFile(fileName string, uploadControl <-chan struct{}, errorFileUpload chan<- string) {
+	defer wg.Done()
 	completeFileName := fmt.Sprintf("./tmp/%s", fileName)
 	fmt.Printf("Uploading file %s to bucket %s", fileName, s3Bucket)
 	f, err := os.Open(completeFileName)
 	if err != nil {
 		fmt.Printf("Error opening file: %s\n", completeFileName)
+		<-uploadControl
+		errorFileUpload <- completeFileName
 		return
 	}
 	defer f.Close()
@@ -68,7 +91,10 @@ func uploadFile(fileName string) {
 	})
 	if err != nil {
 		fmt.Printf("Error uploading file: %s\n", completeFileName)
+		<-uploadControl
+		errorFileUpload <- completeFileName
 		return
 	}
 	fmt.Printf("File uploaded successfully: %s\n", completeFileName)
+	<-uploadControl
 }
