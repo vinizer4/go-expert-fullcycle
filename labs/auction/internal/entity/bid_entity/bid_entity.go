6bd4db81be8701a31cbb4d33316fb80e359c5551
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2025-08-11 23:51:47 -0300

    labs: auction project creating usecases create bid and find bid

--- labs/auction/internal/entity/bid_entity/bid_entity.go
+++ labs/auction/internal/entity/bid_entity/bid_entity.go
@@ -1,6 +1,10 @@
 package bid_entity
 
-import "time"
+import (
+	"context"
+	"time"
+	"vinizer4/go-expert-fullcycle/labs/auction/internal/internal_error"
+)
 
 type Bid struct {
 	Id        string
@@ -8,4 +12,18 @@ type Bid struct {
 	AuctionId string
 	Amount    float64
 	Timestamp time.Time
+}
+
+type BidEntityRepository interface {
+	CreateBid(
+		ctx context.Context,
+		bidEntities []Bid) *internal_error.InternalError
+
+	FindBidByAuctionId(
+		ctx context.Context,
+		auctionId string) ([]Bid, *internal_error.InternalError)
+
+	FindWinningBidByAuctionId(
+		ctx context.Context,
+		auctionId string) (*Bid, *internal_error.InternalError)
 }
