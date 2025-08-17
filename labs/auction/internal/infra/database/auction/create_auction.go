f5e0e422c58fb7594cd1a6beb44a5102bb56df5a
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2025-08-17 19:16:14 -0300

    labs: auction project challenge resolution closing auctions automatic

--- labs/auction/internal/infra/database/auction/create_auction.go
+++ labs/auction/internal/infra/database/auction/create_auction.go
@@ -2,7 +2,10 @@ package auction
 
 import (
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"os"
+	"time"
 	"vinizer4/go-expert-fullcycle/labs/auction/configuration/logger"
 	"vinizer4/go-expert-fullcycle/labs/auction/internal/entity/auction_entity"
 	"vinizer4/go-expert-fullcycle/labs/auction/internal/internal_error"
@@ -47,5 +50,28 @@ func (ar *AuctionRepository) CreateAuction(
 		logger.Error("Error trying to create auction", err)
 		return internal_error.NewInternalServerError("Error trying to create auction")
 	}
+
+	go func() {
+		select {
+		case <-time.After(getAuctionInterval()):
+			update := bson.M{"$set": bson.M{"status": auction_entity.Completed}}
+			filter := bson.M{"_id": auctionEntity.Id}
+			_, err := ar.Collection.UpdateOne(ctx, filter, update)
+			if err != nil {
+				logger.Error("Error trying to update auction status to completed", err)
+				return
+			}
+		}
+	}()
+
 	return nil
+}
+
+func getAuctionInterval() time.Duration {
+	auctionInterval := os.Getenv("AUCTION_INTERVAL")
+	duration, err := time.ParseDuration(auctionInterval)
+	if err != nil {
+		return time.Minute * 5
+	}
+	return duration
 }
